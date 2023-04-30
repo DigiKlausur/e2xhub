baf6fd63887b42d5bfbf2252212d6b6ea98de1f9
Mohammad Wasil <mwasil@outlook.co.id>
2023-04-30 23:33:39 +0200

    fix typo

--- e2xhub/__init__.py
+++ e2xhub/__init__.py
@@ -3,5 +3,5 @@ E2xHub is built on top of JupyterHub using Z2JH to simply course
 management in education.
 """
 
-from ._version import __version__
+from .__version__ import __version__
 from .e2xhub import E2xHub
